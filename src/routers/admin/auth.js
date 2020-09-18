0d19ddbde397c8dd7b1fed20c21d78b234ffd002
Asad Hameed <asadhameed2002@yahoo.com>
2020-09-18 20:48:31 +0200

    Add package express-async-error. after add this package did not write asyncMiddleWare arroundfunction

--- src/routers/admin/auth.js
+++ src/routers/admin/auth.js
@@ -3,14 +3,14 @@ const _ = require('lodash')
 const { validationResult } = require('express-validator');
 const router = express.Router();
 const { User, validation } = require('../../models/user/user.js')
-const asyncMiddleWare = require('../../middleware/async')
 const { requiredEmailExists, requiredValidPasswordForUser } = validation;
 
-router.post('/', [requiredEmailExists, requiredValidPasswordForUser], asyncMiddleWare(async (req, res) => {
+router.post('/', [requiredEmailExists, requiredValidPasswordForUser],async (req, res) => {
     const { errors } = validationResult(req);
     if (errors.length > 0) return res.status(400).send('Error' + errors[0].msg);
     const user = await User.findOne({ email: req.body.email });
     const token = user.generateAuthToken()
-    res.header('x-auth-token', token).send(_.pick(user, ['name', 'email']))
-}));
+   // throw new Error("sfdfjfd")
+   res.header('x-auth-token', token).send(_.pick(user, ['name', 'email']))
+});
 module.exports = router;
