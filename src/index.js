aa979db55205c4cce0e65630aa24f1824b2a9966
Asad Hameed <asadhameed2002@yahoo.com>
2020-09-19 09:23:07 +0200

    add level info in winston mongodb and comment throw new Error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('express-async-errors');
-const winston  = require('winston');
+const winston = require('winston');
 require('winston-mongodb')
 const express = require('express');
 const mongoose = require('mongoose');
@@ -8,16 +8,21 @@ require('dotenv').config();
 const authRouter = require('./routers/admin/auth');
 const userRegistrationRouter = require('./routers/admin/user-registration')
 const customerRouter = require('./routers/customer/customer')
-const error = require('./middleware/error')
+const error = require('./middleware/error');
+const { info } = require('winston');
 
- process.on('uncaughtException' , (ex)=>{
-     console.log(" Exception Got to start the application")
-     winston.error(ex.message, ex)
-     process.exit(1);
-     }) 
+process.on('uncaughtException', (ex) => {
+    console.log(" Exception Got to start the application")
+    winston.error(ex.message, ex)
+    process.exit(1);
+})
 
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-winston.add(new winston.transports.MongoDB({db:'mongodb://localhost/sale-management',options:{ useNewUrlParser: true, useUnifiedTopology: true}}))
+winston.add(new winston.transports.MongoDB({
+    db: 'mongodb://localhost/sale-management',
+    level:'info',
+    options: { useNewUrlParser: true, useUnifiedTopology: true }
+}))
 mongoose.connect('mongodb://localhost/sale-management', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => console.log('Connect with mongo db with sale management'))
     .catch(err => console.log('Error occurs in sale management database', err))
@@ -31,7 +36,7 @@ if (!process.env.JWT_PRIVATE_KEY) {
  * process.on('uncaughtException' , (ex)=>{
  * }) 
  */
-throw new Error("exception happened before application start") 
+//throw new Error("exception happened before application start")
 app.use(express.json());
 app.use('/signin', authRouter)
 app.use('/signup', userRegistrationRouter)
