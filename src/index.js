20b421978791c1754cd8aa1ff85cae875973156c
Asad Hameed <asadhameed2002@yahoo.com>
2020-09-19 09:18:29 +0200

    Uncaught Exceptions during application

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ const userRegistrationRouter = require('./routers/admin/user-registration')
 const customerRouter = require('./routers/customer/customer')
 const error = require('./middleware/error')
 
+ process.on('uncaughtException' , (ex)=>{
+     console.log(" Exception Got to start the application")
+     winston.error(ex.message, ex)
+     process.exit(1);
+     }) 
+
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 winston.add(new winston.transports.MongoDB({db:'mongodb://localhost/sale-management',options:{ useNewUrlParser: true, useUnifiedTopology: true}}))
 mongoose.connect('mongodb://localhost/sale-management', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
@@ -19,6 +25,13 @@ if (!process.env.JWT_PRIVATE_KEY) {
     console.error("FATAl Error: JwtPrivateKey is not define ")
     process.exit(1);
 }
+/****************************************************
+ * If error happened before the application start then winston can not log that error.
+ * catch the exception in start of application and save in winston log
+ * process.on('uncaughtException' , (ex)=>{
+ * }) 
+ */
+throw new Error("exception happened before application start") 
 app.use(express.json());
 app.use('/signin', authRouter)
 app.use('/signup', userRegistrationRouter)
