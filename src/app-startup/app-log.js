86bc7ea7ba98e883775f2e6b45aa4db38f5ded1f
Asad Hameed <asadhameed2002@yahoo.com>
2020-09-19 12:26:08 +0200

    The errors can show in console as well

--- src/app-startup/app-log.js
+++ src/app-startup/app-log.js
@@ -1,5 +1,5 @@
 require('express-async-errors');
-const winston=require('winston');
+const winston = require('winston');
 require('winston-mongodb')
 module.exports = function () {
 
@@ -11,10 +11,10 @@ module.exports = function () {
   );
  */
     winston.exceptions.handle(
+        new winston.transports.Console({ colorize: true, prettyPrint:true}),
         new winston.transports.File({ filename: 'exceptions.log' }));
     process.on('unhandledRejection', (ex) => {
-        throw ex;
-    })
+        throw ex;})
     winston.add(new winston.transports.File({ filename: 'logfile.log' }));
     winston.add(new winston.transports.MongoDB({
         db: 'mongodb://localhost/sale-management',
