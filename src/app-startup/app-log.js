6d05c5802ff446321e90811a76e8b71a8f9660bc
Asad Hameed <asadhameed2002@yahoo.com>
2020-09-19 11:53:49 +0200

    Put all the application log in app-log.js file

---
+++ src/app-startup/app-log.js
@@ -0,0 +1,55 @@
+require('express-async-errors');
+const winston=require('winston');
+require('winston-mongodb')
+module.exports = function () {
+
+    /*************************
+ *  This is handle only caught only uncaughtException. If there is unhandledRejection then winston exception is
+ * not handle it
+    winston.exceptions.handle(
+    new winston.transports.File({ filename: 'exceptions.log' })
+  );
+ */
+    winston.exceptions.handle(
+        new winston.transports.File({ filename: 'exceptions.log' }));
+    process.on('unhandledRejection', (ex) => {
+        throw ex;
+    })
+    winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+    winston.add(new winston.transports.MongoDB({
+        db: 'mongodb://localhost/sale-management',
+        level: 'info',
+        options: { useNewUrlParser: true, useUnifiedTopology: true }
+    }))
+    // process.on('uncaughtException', (ex) => {
+    //     console.log(" Exception Got to start the application")
+    //     winston.error(ex.message, ex)
+    //     process.exit(1);
+    // })
+    // process.on('unhandledRejection', (ex) => {
+    //      console.log(" Exception Got an unhandled Rejection")
+    //     winston.error(ex.message, ex)
+    //      process.exit(1);
+    //     throw ex;
+    // })
+
+
+    /****************************************************
+     * If error happened before the application start then winston can not log that error.
+     * caught the exception in start of application and save in winston log
+     * process.on('uncaughtException' , (ex)=>{
+     * }) 
+     */
+    //throw new Error("exception happened before application start")
+    /***************************
+     * Uncaught the promise rejection
+     * If promise is reject, caught the unhandled rejection
+     * process.on('unhandledRejection', (ex)=>{
+     * })
+     *  const p = Promise.reject(new Error("promise is rejected"))
+     * p.then(()=>console.log("done"))
+     * 
+     */
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,60 +1,15 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb')
 const express = require('express');
 const app = express();
+require('./app-startup/app-log')()
 require('./app-startup/app-routers')(app)
-require('./app-startup/app-database-connection')()
+require('./app-startup/app-db-connection')()
 require('dotenv').config();
 
 
-
-/*************************
- *  This is handle only caught only uncaughtException. If there is unhandledRejection then winston exception is
- * not handle it
- winston.exceptions.handle(
-    new winston.transports.File({ filename: 'exceptions.log' })
-  );
- */
-winston.exceptions.handle(
-    new winston.transports.File({ filename: 'exceptions.log' }));
-
-// process.on('uncaughtException', (ex) => {
-//     console.log(" Exception Got to start the application")
-//     winston.error(ex.message, ex)
-//     process.exit(1);
-// })
-process.on('unhandledRejection', (ex) => {
-    // console.log(" Exception Got an unhandled Rejection")
-    // winston.error(ex.message, ex)
-    // process.exit(1);
-    throw ex;
-})
-
-winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-winston.add(new winston.transports.MongoDB({
-    db: 'mongodb://localhost/sale-management',
-    level: 'info',
-    options: { useNewUrlParser: true, useUnifiedTopology: true }
-}))
-
-/****************************************************
- * If error happened before the application start then winston can not log that error.
- * caught the exception in start of application and save in winston log
- * process.on('uncaughtException' , (ex)=>{
- * }) 
- */
-//throw new Error("exception happened before application start")
-/***************************
- * Uncaught the promise rejection
- * If promise is reject, caught the unhandled rejection
- * process.on('unhandledRejection', (ex)=>{
- * })
- *  const p = Promise.reject(new Error("promise is rejected"))
- * p.then(()=>console.log("done"))
- * 
- */
-
+if (!process.env.JWT_PRIVATE_KEY) {
+    console.error("FATAl Error: JwtPrivateKey is not define ")
+    process.exit(1);
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
